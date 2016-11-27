dd3a8ca30427d8777358122124427fa770df735b
Chris Molina <chrismolina.soccer@gmail.com>
2016-11-27 00:15:38 -0500

    Initial Commit

---
+++ Udemy Swift app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udemy Swift app
+//
+//  Created by Chris Molina on 11/27/16.
+//  Copyright © 2016 Chris Molina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
